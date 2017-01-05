ee9ca018a142fa6efb44ab581f62380074c0047b
fothergilld <fothergilld@gmail.com>
2017-01-05 22:15:12 +0000

    adding ignore file

--- config.py
+++ config.py
@@ -12,16 +12,16 @@ FILE_DIRECTORY = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__fi
 MAIL_SUBJECT = 'Thanks for your purchase'
 
 # Email address for the 'from' field of the generated email
-MAIL_FROM = 'fothergilld@gmail.com'
+MAIL_FROM = 'adress@gmail.com'
 
 # Email server address
 MAIL_SERVER = 'smtp.gmail.com'
 
 # Email server username
-MAIL_USERNAME = 'fothergilld@gmail.com'
+MAIL_USERNAME = 'adress@gmail.com'
 
 # Email server password
-MAIL_PASSWORD = 'T0mwaits'
+MAIL_PASSWORD = '##enter-psw###'
 
 # Email server port
 MAIL_PORT = '465'
@@ -33,16 +33,15 @@ MAIL_USE_SSL = True
 SITE_ADDRESS = 'brokenrecordsband.com'
 
 # Database URI for SQLAlchmey (Default: 'sqlite+pysqlite3:///sqlite3.db')
-SQLALCHEMY_DATABASE_URI = 'mysql://admine3Pwt5d:durkzZ9Hk-xx@127.13.48.2/pi'
+SQLALCHEMY_DATABASE_URI = 'mysql://#user#:#psw#@127.13.48.2/pi'
 
 # Stripe secret key to be used to process purchases
 #STRIPE_SECRET_KEY = 'sk_test_EPuQFdamG8SDE6loD9PVlVOc'
-STRIPE_SECRET_KEY = 'sk_live_wiHmIlBCGL4r7iL6LXYzSG7C'
+STRIPE_SECRET_KEY = '#str_secret_key#'
 
 # Stripe public key to be used to process purchases
-#STRIPE_PUBLIC_KEY = 'pk_test_tE1j955ur757iZnr0upPLk4f'
-STRIPE_PUBLIC_KEY = 'pk_live_9QeCd98NcIScqM3PeX81es5O'
+STRIPE_PUBLIC_KEY = '#str_pub_key#'
 
 #FORM CONFIG
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = '#create-a-key#'
